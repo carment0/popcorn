db91f4231def1561b0d2bf01f20ab14ac829d31a
Calvin Feng <calvinjhfeng@gmail.com>
2018-02-05 02:11:52 -0800

    Added the three major containers, Landing, Navigation, and Recommender

---
+++ frontend/containers/RecommenderPage.jsx
@@ -0,0 +1,14 @@
+/**
+ * @copyright Consilium, 2018
+ * @author Calvin Feng
+ */
+
+import React from 'react';
+
+class Recommender extends React.Component {
+  render() {
+    return <h1>Recommender</h1>;
+  }
+}
+
+export default Recommender;
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,24 +1,74 @@
+/**
+ * @copyright Consilium, 2018
+ * @author Calvin Feng
+ */
+
+// External imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { HashRouter, Route } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import './index.scss';
+
+// Internal imports
+import RecommenderPage from './containers/RecommenderPage';
+import LandingPage from './containers/LandingPage';
+import NavigationBar from './containers/NavigationBar';
+import RootReducer from './reducers';
 
 
 class Application extends React.Component {
+  static propTypes = {
+    history: PropTypes.object.isRequired,
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.history.location.pathname === '/') {
+      this.props.history.push('/welcome');
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.history.location.pathname === '/') {
+      this.props.history.push('/welcome');
+    }
+  }
+
   render() {
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-        <article>
-          <h1>Popcorn</h1>
-        </article>
-      </MuiThemeProvider>
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+            <article className="consilium">
+                <NavigationBar />
+                <Route path="/welcome" component={LandingPage} />
+                <Route path="/recommendations" component={RecommenderPage} />
+            </article>
+        </MuiThemeProvider>
     );
   }
 }
 
+const Router = (props) => (
+  <Provider store={props.store}>
+    <HashRouter>
+      <Route path="/" component={Application} />
+    </HashRouter>
+  </Provider>
+);
+
 document.addEventListener("DOMContentLoaded", () => {
-    ReactDOM.render(<Application />, document.getElementById('react-application'));
+  const preloadedState = {};
+
+  if (window.currentUser) {
+    preloadedState.sessions = { currentUser: window.currentUser };
+    delete window.currentUser;
+  }
+
+  const reduxStore = createStore(RootReducer)
+  ReactDOM.render(<Router />, document.getElementById('react-application'));
 });

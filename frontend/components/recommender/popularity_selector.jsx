3ab9c27fc41e76ee8c701e895666c68985a0b85f
Calvin Feng <calvinjhfeng@gmail.com>
2018-02-12 23:38:12 -0800

    Added popularity sliding selector

--- frontend/components/recommender/popularity_selector.jsx
+++ frontend/components/recommender/popularity_selector.jsx
@@ -3,11 +3,22 @@
  * @author Calvin Feng
  */
 
-import React            from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './popularity_selector.scss';
 
 
 class PopularitySelector extends React.Component {
+  state = {
+    percentile: this.props.movieRatingCountPercentile
+  };
+
+  static propTypes = {
+    movieRatingCountPercentile: PropTypes.number.isRequired,
+    disabled: PropTypes.bool.isRequired,
+    dispatchSetMovieRatingCountPercentile: PropTypes.func.isRequired
+  };
+
   render() {
     return <div />;
   }
